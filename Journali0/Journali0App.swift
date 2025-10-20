d45d1356c40c697a9c1d54515e15ec7053380907
Fai <faialtayeb@iclopud.com>
2025-10-20 10:10:59 +0300

    Initial Commit

---
+++ Journali0/Journali0App.swift
@@ -0,0 +1,17 @@
+//
+//  Journali0App.swift
+//  Journali0
+//
+//  Created by Fai Altayeb on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Journali0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
